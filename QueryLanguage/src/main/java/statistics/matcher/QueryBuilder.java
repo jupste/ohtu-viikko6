cbdbea79c3573fa3f31cb57691a8ae2dcd34c6cd
Jussi Steenari <jussi.steenari@helsinki.fi>
2018-12-09 19:09:15 +0200

    teht 4

---
+++ QueryLanguage/src/main/java/statistics/matcher/QueryBuilder.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package statistics.matcher;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author jussiste
+ */
+public class QueryBuilder {
+
+    private List<Matcher> matchers;
+    private Matcher current;
+
+    public QueryBuilder() {
+        this.matchers = new ArrayList<>();
+    }
+
+    public Matcher matcher() {
+        Matcher[] list = new Matcher[this.matchers.size()];
+        for (int i = 0; i < list.length; i++) {
+            list[i] = matchers.get(i);
+        }
+        return new And(list);
+    }
+
+    public QueryBuilder hasAtLeast(int value, String category) {
+        current = new HasAtLeast(value, category);
+        this.matchers.add(current);
+        return this;
+    }
+
+    public QueryBuilder hasFewerThan(int value, String category) {
+        current = new HasFewerThan(value, category);
+        this.matchers.add(current);
+        return this;
+    }
+
+    public QueryBuilder and(Matcher... matchers) {
+        current = new And(matchers);
+        this.matchers.add(current);
+        return this;
+    }
+
+    public QueryBuilder not(Matcher... matchers) {
+        current = new Not(matchers);
+        this.matchers.add(current);
+        return this;
+    }
+
+    public QueryBuilder or(Matcher... matchers) {
+        current = new Or(matchers);
+        this.matchers.add(current);
+        return this;
+    }
+
+    public QueryBuilder all() {
+        current = new All();
+        this.matchers.add(current);
+        return this;
+    }
+
+    public QueryBuilder playsIn(String team) {
+        current = new PlaysIn(team);
+        this.matchers.add(current);
+        return this;
+    }
+}
